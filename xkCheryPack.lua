3aa921b34d9322495665614392b10993eabe7fdb
BRFX-Shop <brfx.shop@gmail.com>
2026-05-03 15:47:21 +0300

    Update car models prices

--- xkCheryPack.lua
+++ xkCheryPack.lua
@@ -1,4 +1,4 @@
-    ["xk24omoda5"] = {"Audi S3 2017 Chrome",163250, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24omoda5.png?raw=true' width='360' height='240'/>"},
-    ["xk24arrizo8"] = {"Audi Q5 2018",139999, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24arrizo8.png?raw=true' width='360' height='240'/>"},
-    ["xk24tiggo8"] = {"Audi RS3 2018",223900, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24tiggo8.png?raw=true' width='360' height='240'/>"},
-    ["xk25tiggo9"] = {"Audi R8 2020",844900, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk25tiggo9.png?raw=true' width='360' height='240'/>"},
+    ["xk24omoda5"] = {"Chery Omoda 5 2024",151990, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24omoda5.png?raw=true' width='360' height='240'/>"},
+    ["xk24arrizo8"] = {"Chery Arrizo 8 2024",159990, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24arrizo8.png?raw=true' width='360' height='240'/>"},
+    ["xk24tiggo8"] = {"Chery Tiggo 8 2024",204990, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24tiggo8.png?raw=true' width='360' height='240'/>"},
+    ["xk25tiggo9"] = {"Chery Tiggo 9 2025",217990, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk25tiggo9.png?raw=true' width='360' height='240'/>"},
