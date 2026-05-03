4a3d54ab43fdc13430fc27af80870ffb6786463b
BRFX-Shop <brfx.shop@gmail.com>
2026-05-03 15:44:46 +0300

    Add new Chery car entries to xkCheryPack.lua

---
+++ xkCheryPack.lua
@@ -0,0 +1,4 @@
+    ["xk24omoda5"] = {"Audi S3 2017 Chrome",163250, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24omoda5.png?raw=true' width='360' height='240'/>"},
+    ["xk24arrizo8"] = {"Audi Q5 2018",139999, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24arrizo8.png?raw=true' width='360' height='240'/>"},
+    ["xk24tiggo8"] = {"Audi RS3 2018",223900, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24tiggo8.png?raw=true' width='360' height='240'/>"},
+    ["xk25tiggo9"] = {"Audi R8 2020",844900, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk25tiggo9.png?raw=true' width='360' height='240'/>"},
