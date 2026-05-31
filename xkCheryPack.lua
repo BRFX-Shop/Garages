1a3dcb4b1175e3a8dc4d47d19913662660307cc6
BRFX-Shop <brfx.shop@gmail.com>
2026-06-01 02:13:43 +0300

    Update xkCheryPack.lua

--- xkCheryPack.lua
+++ xkCheryPack.lua
@@ -2,3 +2,4 @@
     ["xk24arrizo8"] = {"Chery Arrizo 8 2024",159990, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24arrizo8.png?raw=true' width='360' height='240'/>"},
     ["xk24tiggo8"] = {"Chery Tiggo 8 2024",204990, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24tiggo8.png?raw=true' width='360' height='240'/>"},
     ["xk25tiggo9"] = {"Chery Tiggo 9 2025",217990, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk25tiggo9.png?raw=true' width='360' height='240'/>"},
+    ["xk26JaecooJ7"] = {"Chery Jaecoo J7 2026",202990, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk26jaecooj7.png?raw=true' width='360' height='240'/>"}
