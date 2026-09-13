5899994e5bfd1f3a91957f2305c6c2a7aab787bd
BRFX-Shop <brfx.shop@gmail.com>
2026-09-13 08:46:10 +0300

    Add new Rubicon models for 2026 and 2027

---
+++ RubiconV2.lua
@@ -0,0 +1,4 @@
+    ["xk26Rubicon392"] = {"Rubicon 392 Final Edition 2026",670000, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk26Rubicon392.png?raw=true' width='360' height='340'/>"},
+    ["xk27Rubicon392"] = {"Rubicon 392 Moab 2027",779900, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk27Rubicon392.png?raw=true' width='360' height='340'/>"},
+    ["xk27Rubicon4xe"] = {"Rubicon 4xe 2027",460000, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk27Rubicon4xe.png?raw=true' width='360' height='340'/>"},
+    ["xk27Rubicon"] = {"Rubicon 2027",580000, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk27Rubicon.png?raw=true' width='360' height='340'/>"},
