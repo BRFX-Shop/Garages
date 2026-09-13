b54d51d1d9587b29a42a0661dcb2dd99f757017e
BRFX-Shop <brfx.shop@gmail.com>
2026-09-13 09:50:12 +0300

    Create InteractiveV1.lua

---
+++ InteractiveV1.lua
@@ -0,0 +1,3 @@
+    ["xk23bmwx6"] = {"BMW X6 40i 2023",478900, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk23bmwx6.png?raw=true' width='360' height='240'/>"},
+    ["xk26rubicon"] = {"Rubicon 2026",513000, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk26rubicon.png?raw=true' width='360' height='240'/>"},
+    ["xk24superblk"] = {"Superb L&K 2024",174000, "<img src='https://github.com/BRFX-Shop/Cars/blob/main/xk24superblk.png?raw=true' width='360' height='240'/>"}
